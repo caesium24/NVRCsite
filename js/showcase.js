3c7fdbc7b956b9fae253b3200bcfef2bc851045f
caesium24 <57577574+caesium24@users.noreply.github.com>
2020-04-26 17:47:56 -0500

    modified:   contracts.html 	modified:   css/_aboutsection.scss 	new file:   css/_content.scss 	modified:   css/_footer.scss 	modified:   css/_login.scss 	modified:   css/_mobilenav.scss 	modified:   css/_showcase.scss 	modified:   css/_solutions.scss 	modified:   css/_vars.scss 	modified:   css/nvrc.css 	modified:   css/nvrc.css.map 	modified:   css/nvrc.scss 	deleted:    img/battlefield.jpg 	deleted:    img/first.bmp 	deleted:    img/second.bmp 	deleted:    img/ship2.jpg 	deleted:    img/ship3.jpg 	deleted:    img/ship4.jpg 	deleted:    img/test0.jpg 	deleted:    img/test1.bmp 	deleted:    img/test2.bmp 	deleted:    img/test3.bmp 	deleted:    img/third.bmp 	modified:   index.html 	modified:   js/showcase.js 	modified:   login.html

--- js/showcase.js
+++ js/showcase.js
@@ -1,8 +1,10 @@
 const imgGrow = () => {
     const topLeft = document.querySelector('.top-left');
     const topRight = document.querySelector('.top-right');
-    const topLefth1 = document.querySelector('.top-left h1');
-    const topRighth1 = document.querySelector('.top-right h1');
+    const topLeftH1 = document.querySelector('#topLeftH1');
+    const topRightH1 = document.querySelector('#topRightH1');
+    const topLeftH2 = document.querySelector('#topLeftH2');
+    const topRightH2 = document.querySelector('#topRightH2');
     const topLeftbtn = document.querySelector('#btn1');
     const topRightbtn = document.querySelector('#btn2');
     const textLeft = document.querySelector('.top-left h1');
@@ -13,11 +15,15 @@ const imgGrow = () => {
         topRight.style.width = ".1%";
         textRight.style.opacity = "0";
         topLeftbtn.style.opacity = "1";
+        topLeftH1.style.fontSize = "5rem";
+        topLeftH2.style.fontSize = "3rem";
     });
     topLeft.addEventListener('mouseleave', () => {
         topLeft.style.width = "49.95%";
         topRight.style.width = "49.95%";
         topLeftbtn.style.opacity = "0";
+        topLeftH1.style.fontSize = "3rem";
+        topLeftH2.style.fontSize = "1.5rem";
         setTimeout(() => {
             textRight.style.opacity = "1";
         }, 300);
@@ -27,17 +33,19 @@ const imgGrow = () => {
         topLeft.style.width = ".1%";
         textLeft.style.opacity = "0";
         topRightbtn.style.opacity = "1";
-
+        topRightH1.style.fontSize = "5rem";
+        topRightH2.style.fontSize = "3rem";
     });
     topRight.addEventListener('mouseleave', () => {
         topRight.style.width = "49.95%";
         topLeft.style.width = "49.95%";
         topRightbtn.style.opacity = "0";
+        topRightH1.style.fontSize = "3rem";
+        topRightH2.style.fontSize = "1.5rem";
         setTimeout(() => {
             textLeft.style.opacity = "1";
         }, 300);
     });
-
 };
 
 imgGrow();
